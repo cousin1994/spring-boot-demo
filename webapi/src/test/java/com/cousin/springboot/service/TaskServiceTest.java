a6bb37e6c0c858b4eb3cf0fd9cb29f9eae5c39c6
cousin <773807943@qq.com>
2016-12-05 20:42:44 +0800

    增加忽略

---
+++ webapi/src/test/java/com/cousin/springboot/service/TaskServiceTest.java
@@ -0,0 +1,32 @@
+package com.cousin.springboot.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by cousin
+ * CreatedTime 2016/12/422:14
+ */
+@RunWith(SpringRunner.class)
+public class TaskServiceTest {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    public void doTaskOne() throws Exception {
+    }
+
+    @Test
+    public void doTaskTwo() throws Exception {
+
+    }
+
+    @Test
+    public void doTaskThree() throws Exception {
+
+    }
+
+}
