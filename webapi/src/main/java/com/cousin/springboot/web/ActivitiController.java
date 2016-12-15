5d4e15993ee589623741efdc4efb43917d170a24
cousin <773807943@qq.com>
2016-12-16 00:05:03 +0800

    增加删除已经部署的activiti

--- webapi/src/main/java/com/cousin/springboot/web/ActivitiController.java
+++ webapi/src/main/java/com/cousin/springboot/web/ActivitiController.java
@@ -5,6 +5,7 @@ import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
@@ -13,9 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -89,5 +93,42 @@ public class ActivitiController {
         return "redirect:/activiti/tasks";
     }
 
+    /**
+     * 删除部署的流程，级联删除流程实例
+     *
+     * @param deploymentId 流程部署ID
+     */
+    @RequestMapping(value = "/processes/delete/{deploymentId}")
+    public String deleteProcessDefinition(@PathVariable("deploymentId") String deploymentId) {
+        repositoryService.deleteDeployment(deploymentId, true);
+        return "redirect:/activiti/processes";
+    }
+
+
+    /**
+     * 读取流程资源
+     *
+     * @param processDefinitionId 流程定义ID
+     * @param resourceName        资源名称
+     */
+    @RequestMapping(value = "/read-resource")
+    public void readResource(@RequestParam("pdid") String processDefinitionId, @RequestParam("resourceName") String resourceName, HttpServletResponse response)
+            throws Exception {
+
+
+        ProcessDefinitionQuery pdq = repositoryService.createProcessDefinitionQuery();
+        ProcessDefinition pd = pdq.processDefinitionId(processDefinitionId).singleResult();
+
+        // 通过接口读取
+        InputStream resourceAsStream = repositoryService.getResourceAsStream(pd.getDeploymentId(), resourceName);
+
+
+        // 输出资源内容到相应对象
+        byte[] b = new byte[1024];
+        int len = -1;
+        while ((len = resourceAsStream.read(b, 0, 1024)) != -1) {
+            response.getOutputStream().write(b, 0, len);
+        }
+    }
 
 }
