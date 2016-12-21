e1c4c0892b9fea95b1eed3b6d952f4366f5b96c0
cousin <773807943@qq.com>
2016-12-21 09:01:17 +0800

    移动静态资源到static包

--- webapi/src/main/java/com/cousin/springboot/web/LoginController.java
+++ webapi/src/main/java/com/cousin/springboot/web/LoginController.java
@@ -1,5 +1,6 @@
 package com.cousin.springboot.web;
 
+import com.cousin.springboot.model.pojo.UserInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -7,11 +8,10 @@ import org.apache.shiro.authc.UnknownAccountException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 /**
  * 登录controller
@@ -28,20 +28,24 @@ public class LoginController {
         return "/index";
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String login(){
-        String user = (String) SecurityUtils.getSubject().getPrincipal();
-        if (StringUtils.isNotBlank(user)) {
-            SecurityUtils.getSubject().getSession().setTimeout(-1000L);
-            return "index";
-        }
-        return "login";
-    }
+//    @RequestMapping(value = "/login",method = RequestMethod.GET)
+//    public String login(){
+//        String user = (String) SecurityUtils.getSubject().getPrincipal();
+//        if (StringUtils.isNotBlank(user)) {
+//            SecurityUtils.getSubject().getSession().setTimeout(-1000L);
+//            return "index";
+//        }
+//        return "login";
+//    }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(HttpServletRequest request, Map<String, Object> map){
+    @RequestMapping(value = "/login")
+    public String login(HttpServletRequest request, Model model){
         logger.info("开始接受登录页面操作");
-
+        UserInfo user = (UserInfo) SecurityUtils.getSubject().getPrincipal();
+        if (user!=null) {
+            SecurityUtils.getSubject().getSession().setTimeout(-1000L);
+            return "/index";
+        }
 
         String exception = (String)request.getAttribute("shiroLoginFailure");
 
@@ -63,7 +67,9 @@ public class LoginController {
                 logger.error("else -- >" + exception);
             }
         }
-        map.put("msg", msg);
+        if(StringUtils.isNotBlank(msg)) {
+            model.addAttribute("msg", msg);
+        }
         // 此方法不处理登录成功,由shiro进行处理.
         return "/login";
 
